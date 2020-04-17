9cca891f25dd89fa52ee3cf1864e00a6fdfd6db1
Daniel Nogueira de Oliveira <dan.knightcode@gmail.com>
2020-04-17 19:29:52 -0300

    Configurado telas com barra de navegação inferior

--- mobile/src/pages/Home/index.js
+++ mobile/src/pages/Home/index.js
@@ -1,9 +1,62 @@
-import React from 'react';
-import {View,Text} from 'react-native';
-export default function Home(){
-    return (
-        <View style={{flex:1,paddingTop: 50,}}>
-            <Text>Ola home screen</Text>
-        </View>
-    );
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+function PersonScreen(){
+  return(
+    <View style={{backgroundColor:'gray', flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Person!</Text>
+    </View>
+  );
+}
+function HomeScreen() {
+  return (
+    <View style={{ backgroundColor:'gray',flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
+    </View>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={{backgroundColor:'gray', flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <Tab.Navigator
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused, color, size }) => {
+        let iconName;
+
+        if (route.name === 'Home') {
+          iconName = 'md-home';
+        } else if (route.name === 'Settings') {
+          iconName = 'md-settings';
+        }else if(route.name=='User'){
+            iconName = 'ios-person';
+        }
+
+        // You can return any component that you like here!
+        return <Ionicons name={iconName} size={size} color={color} />;
+      },
+    })}
+    tabBarOptions={{
+      activeTintColor: 'yellow',
+      inactiveTintColor: 'gray',
+      style:{
+        backgroundColor:'black',
+      }
+    }}
+      >
+      <Tab.Screen name="User" component={PersonScreen}/>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
+  );
 }
