25cafbe01e4423ea06c0816b7afa334608ad835e
Daniel Nogueira de Oliveira <dan.knightcode@gmail.com>
2020-04-20 15:16:58 -0300

    Configuração de rotas para a tablea Users

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,13 @@
 const express = require('express');
 
 const routes = express.Router();
-
+const UserControlls = require('./controllers/UserControlls');
 routes.get('/',(req,res)=>{
-    res.send("<h1>Bem vindo ao backend da aplicação I need a Duo");
+    res.send("<h1>Bem vindo ao backend da aplicação I need a Duo</h1>");
 });
-
+routes.get('/userexists',UserControlls.userExists);
+routes.get('/users/search',UserControlls.indexByEloAndRole);
+routes.get('/users',UserControlls.index);
+routes.post('/user',UserControlls.create);
+routes.get('/users/:role',UserControlls.indexRole);
 module.exports = routes;
